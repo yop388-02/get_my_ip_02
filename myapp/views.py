ce4a65542ae1ebd676f0c875ff6f9b2523ddec75
yop388 <parfait87y@gmail.com>
2024-07-10 12:10:03 +0000

    first commit

--- myapp/views.py
+++ myapp/views.py
@@ -27,6 +27,6 @@ def show_ip_addresses(request):
     except requests.RequestException:
         public_ip = 'Non disponible'
 
-    nom, prenoms = get_client_name("Mon_Nom", "Mon_prénom")
+    nom, prenoms = get_client_name("Y_a_o", "O_thnie_l")
 
     return render(request, 'show_ips.html', {"nom" : nom, 'prenoms' : prenoms, 'private_ip': private_ip, 'public_ip': public_ip})
